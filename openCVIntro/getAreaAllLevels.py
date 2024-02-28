339e4fb7949f2d4796e47018deeb73b4f535c98a
tg <kirill.pankov.20144@gmail.com>
2024-02-28 18:36:35 +0300

    Fixed part of the problem in openCVIntro/getAreaAllLevels.py

--- openCVIntro/getAreaAllLevels.py
+++ openCVIntro/getAreaAllLevels.py
@@ -3,44 +3,45 @@ import cv2
 import numpy as np
 
 
-def view_image(image):
+def view_image(img):
     cv2.namedWindow('Display')
-    cv2.imshow('Display', image)
+    cv2.imshow('Display', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
-def get_area_of_each_gray_level(im):
-    ## преобразование изображения к оттенкам серого (обязательно делается до оконтуривания)
-    image = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+def grayscale_17_levels(img):
+    # important to do before ocunturization
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     output = []
     high = 255
+    low = high
     first = True
-    while (1):
-        low = high - 15
-        if first == False:
-            break
-        # Делаем значения выше уровня серого чёрными. Так они не будут обнаруживаться
-        to_be_black_again_low = np.array([high])
-        to_be_black_again_high = np.array([255])
-        curr_mask = cv2.inRange(image, to_be_black_again_low,
-                                to_be_black_again_high)
-        image[curr_mask > 0] = (0)
 
-        # Делаем значения этого уровня белыми. Так мы рассчитаем их площадь
+    while low > 0:
+        low = high - 15
+        if not first:
+            lower = np.array([high])
+            higher = np.array([255])
+            # cv2. inRange() finds from gray pixels which are brighter than lower and grayer than higher
+            curr_mask = cv2.inRange(image, lower, higher)
+            image[curr_mask > 0] = 0
+            print(img)
+        # ret - minimum which was used for binarization (initially 240)
         ret, threshold = cv2.threshold(image, low, 255, 0)
-        contours, hirerchy = cv2.findContours(threshold,
-                                              cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-        if (len(contours) > 0):
+        # list of contours and their hierarchy
+        contours, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+
+        if len(contours) > 0:
             output.append([cv2.contourArea(contours[0])])
-            cv2.drawContours(im, contours, -1, (0, 0, 255), 3)
+            cv2.drawContours(img, contours, -1, (0, 0, 255), 3)
             high -= 15
             first = False
-        if (low == 0):
-            break
-    return output
+
+    print(output)
+    return img
 
 
 image = cv2.imread('ombre.jpg')
-print(get_area_of_each_gray_level(image))
+image = grayscale_17_levels(image)
 view_image(image)
